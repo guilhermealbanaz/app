0c9f60f801861afb0c0c81e56eae37567f2dd2c0
Guilherme Albanaz <galbanaz@softexpert.com>
2023-09-19 09:00:16 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import './Game.css'; 
-import React, { useState, useEffect, useCallback } from "react";
+import './Game.css';
+import React, { useState, useEffect, useCallback, useRef } from "react";
 
 // cor que deve ser adivinhada
 const CurrentColor = React.memo(({ color }) => {
   return <div style={{ backgroundColor: color }} className='ColorBox'>Guess this color</div>;
 });
+
+const ResetAllData = ({onClick}) => {
+	return <button onClick={onClick}>Reset All Data</button>
+}
 
 // opções
 const ColorOptions = React.memo(({ options, onAnswer }) => {
@@ -20,11 +24,26 @@ function Game() {
   const [currentColor, setCurrentColor] = useState(null);
   const [options, setOptions] = useState([]);
   const [score, setScore] = useState(0);
-  const [highScore, setHighScore] = useState(() => Number(localStorage.getItem('highScore')) || 0);
+  const [highScore, setHighScore] = useState(() => Number(localStorage.getItem('high')) || 0);
   const [gameStatus, setGameStatus] = useState("notStarted");
   const [timeLeft, setTimeLeft] = useState(10);
-  const [history, setHistory] = useState([])
- 
+  const [history, setHistory] = useState([]);
+  const [difficulty, setDifficulty] = useState(3);
+  const scoreRef = useRef(score);
+
+  const resetData = () => {
+	localStorage.clear();
+	setHighScore(0);
+	setScore(0);
+	setGameStatus('notStarted');
+	setHistory([]);
+	setDifficulty(3);
+  }
+
+  const handleDifficultyChange = useCallback((newDifficulty) => {
+    setDifficulty(newDifficulty);
+  }, []);
+
   const generateColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   };
@@ -32,8 +51,8 @@ function Game() {
   const nextRound = useCallback(() => {
     const newColor = generateColor(); // gera cor aleatória
     setCurrentColor(newColor); // adiciona na cor que deve ser acertada
-    const newOptions = [newColor]; // adiciona no array 
-    while (newOptions.length < 3) {  // enquanto as opções forem menor que 3 adiciona mais opções aleatórias no array, se a opção por coincidência ja existir não adiciona.
+    const newOptions = [newColor]; // adiciona no array
+    while (newOptions.length < difficulty) {  // enquanto as opções forem menor que 3 adiciona mais opções aleatórias no array, se a opção por coincidência ja existir não adiciona.
       const option = generateColor();
       if (!newOptions.includes(option)) {
         newOptions.push(option);
@@ -41,9 +60,10 @@ function Game() {
     }
     // embaralha as opções
     setOptions(newOptions.sort(() => Math.random() - 0.5));
-  }, []);
+  }, [difficulty]);
 
-  const startGame = () => {
+  const startGame = useCallback(() => {
+    setHistory([]);
     setScore(0); // restarta o score
     setGameStatus('inProgress'); // muda status ao começar
     setTimeLeft(10); // restarta o tempo quando inicia o jogo
@@ -51,9 +71,14 @@ function Game() {
 
     // depois de 30s muda o status do game para terminado
     setTimeout(() => {
-      setGameStatus('ended');
-    }, 30000);
-  };
+		setHighScore((currentHighScore) => {
+			const newHighScore = scoreRef.current > currentHighScore ? scoreRef.current : currentHighScore;
+			localStorage.setItem('highScore', newHighScore);
+			return newHighScore;
+		  });
+		  setGameStatus('ended');
+	}, 30000);
+	}, [nextRound]);
 
   const handleAnswer = useCallback((answer) => {
     const isCurrentColor = answer === currentColor;
@@ -66,26 +91,24 @@ function Game() {
     setTimeLeft(10);
     nextRound();
 
-    const timeTaken = 10 - timeLeft;
-
     setHistory(prevHistory => [...prevHistory, { answer, isCurrentColor: isCurrentColor }])
-  }, [gameStatus, currentColor, nextRound]);
+  }, [currentColor, gameStatus, nextRound]);
 
   useEffect(() => {
-    if (score > highScore) {
-      setHighScore(score);
-      localStorage.setItem('highScore', score);
-    }
+    let timerId;
+	if (timeLeft > 0) {
+		timerId = setTimeout(() => {
+		setTimeLeft(timeLeft - 1);
+		}, 1000);
+	} else if (gameStatus === 'inProgress') {
+		handleAnswer(null); // tratado como uma resposta errada se o tempo acabar
+	}
+  	return () => clearTimeout(timerId);
+  }, [timeLeft, gameStatus, handleAnswer, nextRound]);
 
-    if (timeLeft > 0) {
-      const timerId = setTimeout(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 1000);
-      return () => clearTimeout(timerId); // limpa o temporizador se o componente for desmontado
-    } else if (gameStatus === 'inProgress') {
-      handleAnswer(null); // tratado como uma resposta errada se o tempo acabar
-    }
-  }, [timeLeft, gameStatus, highScore, score, handleAnswer, nextRound]);
+  useEffect(() => {
+	scoreRef.current = score;
+  }, [score]);
 
   return (
     <div className="App">
@@ -106,18 +129,28 @@ function Game() {
             <p>High Score: {highScore}</p>
             <p>Score: {score}</p>
             {gameStatus === "notStarted" && (
-                <button onClick={startGame}>Start</button>
-            )}
+                <>
+					<button onClick={startGame}>Start</button>
+					<div>
+						<button style={{ cursor: 'pointer', backgroundColor: difficulty === 3 ? '#f2efd3' : 'initial' }} onClick={() => handleDifficultyChange(3)}>Fácil</button>
+						<button style={{ cursor: 'pointer', backgroundColor: difficulty === 4 ? '#f2efd3' : 'initial' }} onClick={() => handleDifficultyChange(4)}>Médio</button>
+						<button style={{ cursor: 'pointer', backgroundColor: difficulty === 5 ? '#f2efd3' : 'initial' }} onClick={() => handleDifficultyChange(5)}>Difícil</button>
+					</div>
+					<ResetAllData onClick={resetData}/>
+				</>
+			)}
             {gameStatus === "inProgress" && (
                 <>
                     <CurrentColor color={currentColor} />
                     <ColorOptions options={options} onAnswer={handleAnswer} />
                 </>
             )}
+
             {gameStatus === "ended" && (
                 <>
                     <p>Game Over!</p>
                     <button onClick={startGame}>Play Again</button>
+					<ResetAllData onClick={resetData}/>
                 </>
             )}
         </div>
